6e68b0bb649fe4c937ccb69a2b327e5429846612
Yitzhak <yspielb@gmail.com>
2021-03-30 10:43:00 +0300

    upd

--- run_bootstrap.py
+++ run_bootstrap.py
@@ -140,10 +140,6 @@ if info['advice_flg']:
     if info['PRIOR']:
         advice_net = NetWithPrior(advice_net, prior_net, info['PRIOR_SCALE'])
     advice_net.load_state_dict(model_dict['policy_net_state_dict'])
-<<<<<<< HEAD
-
-=======
->>>>>>> b6a39a1a6c0cf5a886269b6b67233bd0230a82ce
 action_getter = ActionGetter(n_actions=env.num_actions,
                              policy_net=policy_net,
                              random_seed=info['seed_expl'],
