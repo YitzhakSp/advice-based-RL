d643cebc5b95cf7237dbf2812eeb23a857263fca
Yitzhak <yspielb@gmail.com>
2021-06-23 16:16:38 +0300

    upd

--- process_results/plot_advice_budget.py
+++ process_results/plot_advice_budget.py
@@ -5,21 +5,29 @@ import json
 import matplotlib.pyplot as plt
 
 #thismodel_dir='simulations/no_advice'
-thismodel_dir='simulations/advice/limited/0.04'
+files=[#('simulations/no_advice/avg_rewards_smooth.npy','no_adv'),
+    ('../simulations/advice/limited/0.04/crityes/avg_advice_cnt.npy','crityes'),
+      ('../simulations/advice/limited/0.04/critno/avg_advice_cnt.npy','critno'),
+      ('../simulations/advice/limited/0.04/critxuncert/avg_advice_cnt.npy','critxuncert')
+]
 avg=True
+'''
 print('loading performance data from '+ thismodel_dir)
 with open(thismodel_dir+'/perf_3.json', 'r') as f:
     perf = json.load(f)
+'''
 mypl=plt
 mypl.ylabel('num_advice')
 #mypl.title('Pong_advice')
 #mypl.plot(perf['steps'],perf['advice_cnt'])
 if avg:
-    print("plotting average advice_budg ...")
-    mypl.xlabel('episodes')
-    avg_advice_cnt = np.load(thismodel_dir+'/avg_advice_cnt.npy')
-    advice_cum=np.cumsum(avg_advice_cnt)
-    mypl.plot(advice_cum)
+    for f in files:
+        print("plotting average advice_budg ...")
+        mypl.xlabel('episodes')
+        avg_advice_cnt = np.load(f[0])
+        advice_cum=np.cumsum(avg_advice_cnt)
+        mypl.plot(advice_cum,label=f[1])
+        plt.legend()
 else:
     print("plotting advice_budget for 1 run ...")
     max_steps = 2.5e6
@@ -30,5 +38,5 @@ else:
     advice_cum=np.cumsum(np.array(perf['advice_cnt']))
     mypl.plot(perf['steps'],advice_cum)
 mypl.grid(True)
-mypl.savefig('plots/num_advice.png')
+mypl.savefig('../plots/num_advice.png')
 a=7
--- process_results/plot_multiple_rewards.py
+++ process_results/plot_multiple_rewards.py
@@ -5,9 +5,7 @@ eval_freq=2 #evaluation episode once every x episodes
 files=[#('simulations/no_advice/avg_rewards_smooth.npy','no_adv'),
     ('../simulations/advice/limited/0.04/crityes/avg_rewards_smooth.npy','crityes'),
       ('../simulations/advice/limited/0.04/critno/avg_rewards_smooth.npy','critno'),
-      ('../simulations/advice/unlimited_hardtrh/0.04/avg_rewards_smooth.npy', 'unlimited adv'),
-      ('../simulations/no_advice/avg_rewards_smooth.npy', 'no adv'),
-
+      ('../simulations/advice/limited/0.04/critxuncert/avg_rewards_smooth.npy','critxuncert'),
 ]
 for f in files:
     y = np.load(f[0])
