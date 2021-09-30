c3cfba2a48ef4c87c6066f2edf84ee9d60f58e90
YitzhakSp <yspielbggg>
2021-09-30 12:56:11 +0300

    upd

--- process_results/plot_advice_consumption.py
+++ process_results/plot_advice_consumption.py
@@ -4,22 +4,16 @@ from other_utils import *
 import json
 import matplotlib.pyplot as plt
 
-#thismodel_dir='simulations/no_advice'
-files=[('../simulations/advice/limited/0.04/critno/avg_advice_cnt.npy','BDQN-adv'),
-('../simulations/advice/limited/0.04/crityes/avg_advice_cnt.npy','BDQN-crit1'),
-('../simulations/advice/limited/0.04/critxuncert/avg_advice_cnt.npy','BDQN-crit2')
-]
-avg=True
-'''
-print('loading performance data from '+ thismodel_dir)
-with open(thismodel_dir+'/perf_3.json', 'r') as f:
-    perf = json.load(f)
-'''
+avg=False
 mypl=plt
 mypl.ylabel('num_advice')
 #mypl.title('Pong_advice')
 #mypl.plot(perf['steps'],perf['advice_cnt'])
 if avg:
+    files = [('../simulations/advice/limited/0.04/critno/avg_advice_cnt.npy', 'BDQN-adv'),
+             ('../simulations/advice/limited/0.04/crityes/avg_advice_cnt.npy', 'BDQN-crit1'),
+             ('../simulations/advice/limited/0.04/critxuncert/avg_advice_cnt.npy', 'BDQN-crit2')
+             ]
     mypl.xlabel('episodes')
     mypl.xlim(0, 1070)
     for f in files:
@@ -31,14 +25,27 @@ if avg:
         #mypl.axhline(150000, linestyle='--')
         plt.legend()
 else:
+    perf_file = '../simulations/Pong/advice/limited/0.04/critno/perf_1.son'
     print("plotting advice_budget for 1 run ...")
+    '''
     max_steps = 2.5e6
     mypl.xlabel('steps')
     mypl.xlim(right=max_steps)
-    with open(thismodel_dir+'/perf_3.json', 'r') as f:
+    '''
+    with open(perf_file, 'r') as f:
         perf = json.load(f)
-    advice_cum=np.cumsum(np.array(perf['advice_cnt']))
-    mypl.plot(perf['steps'],advice_cum)
+    advice_cnt=np.array(perf['advice_cnt'])
+    advice_cum=np.cumsum(advice_cnt)
+    mypl.subplot(1, 2, 1)
+    mypl.plot(perf['steps'],advice_cum,label='cummulative advice')
+    mypl.legend()
+    steps=np.array(perf['steps'])
+    steps_in_ep=steps[1:]-steps[:-1]
+    steps_in_ep=np.concatenate((np.array([steps[0]]),steps_in_ep))
+    advice_ratio=advice_cnt/steps_in_ep
+    mypl.subplot(1, 2, 2)
+    mypl.plot(perf['steps'],advice_ratio,label='advice ratio')
+    mypl.legend()
 mypl.grid(True)
-mypl.savefig('../plots/num_advice.png')
+mypl.savefig('num_advice.png')
 a=7
